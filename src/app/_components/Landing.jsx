3028fe7c6406ba5b9826bea8d4c8a65c5108fa54
csajan <csajan360@gmail.com>
2024-10-18 12:36:17 +0100

    Added routing and random css

---
+++ src/app/_components/Landing.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import styles from "../landing.module.css";
+
+function Landing() {
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.heading}>Spooky landing</h3>
+    </div>
+  );
+}
+
+export default Landing;
